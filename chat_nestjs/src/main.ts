2be73d1e58a4af45f6ea1b73f65924c83f0a9e6d
knowave <kwaktaemin98@gmail.com>
2024-09-08 17:25:00 +0900

    swagger

--- chat_nestjs/src/main.ts
+++ chat_nestjs/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Chat For NestJS API')
+    .setDescription('The Chatting Service API Description')
+    .setVersion('1.0')
+    .addTag('Chat')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
 }
 bootstrap();
