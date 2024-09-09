5f2c57720413c12635313fddb51b6aad48ca1b80
knowave <kwaktaemin98@gmail.com>
2024-09-09 17:06:58 +0900

    user repository

---
+++ chat_nestjs/src/user/user.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(private readonly dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async getUserById(id: number): Promise<User> {
+    return await this.createQueryBuilder('user')
+      .where('user.id = :id', { id })
+      .getOne();
+  }
+}
