6d98ee482a5601394e55c0c2785056e5b4bb32e0
knowave <kwaktaemin98@gmail.com>
2024-09-09 16:41:39 +0900

    user entity

--- chat_nestjs/src/user/entities/user.entity.ts
+++ chat_nestjs/src/user/entities/user.entity.ts
@@ -1,1 +1,53 @@
-export class User {}
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
+import { BaseEntity } from 'src/common/base.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column({
+    type: 'varchar',
+    length: 100,
+    comment: '사용자 이메일',
+    unique: true,
+  })
+  @IsEmail()
+  @IsNotEmpty({ message: '이메일을 입력해주세요.' })
+  email: string;
+
+  @Column({ type: 'varchar', length: 10, comment: '사용자 이름' })
+  @IsString()
+  @IsNotEmpty({ message: '이름을 입력해주세요' })
+  @Matches(/^[가-힣a-zA-Z]{2,10}$/, {
+    message: '이름은 한글, 영문 2~10자로 입력해주세요',
+  })
+  username: string;
+
+  @Column({ type: 'varchar', length: 20, comment: '사용자 닉네임' })
+  @IsString()
+  @IsNotEmpty({ message: '닉네임을 입력해주세요' })
+  @Matches(/^[가-힣a-zA-Z]{2,20}$/, {
+    message: '닉네임은 한글, 영문 2~20자로 입력해주세요',
+  })
+  nickname: string;
+
+  @Column({ type: 'varchar', comment: '사용자 비빌번호' })
+  @IsNotEmpty({ message: '비밀번호를 입력해주세요' })
+  @Matches(
+    /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/,
+    {
+      message:
+        '비밀번호는 영문 대소문자, 숫자, 특수문자를 포함한 8~20자로 입력해주세요',
+    },
+  )
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    comment: '사용자 token',
+    nullable: true,
+  })
+  token: string;
+
+  @Column()
+  isDeleted: boolean;
+}
