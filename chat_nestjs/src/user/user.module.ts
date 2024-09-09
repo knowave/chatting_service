a47b424bdeeec95c66f0caee113697587f5a9204
knowave <kwaktaemin98@gmail.com>
2024-09-09 17:17:50 +0900

    inject class

--- chat_nestjs/src/user/user.module.ts
+++ chat_nestjs/src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserRepository } from './user.repository';
 
 @Module({
+  providers: [UserService, UserRepository],
   controllers: [UserController],
-  providers: [UserService]
+  exports: [UserService],
 })
 export class UserModule {}
