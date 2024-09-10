b70b73d401c18b51f5be169f2b0c1d717c860022
knowave <kwaktaemin98@gmail.com>
2024-09-10 17:40:30 +0900

    상대방의 Id 값 추가

---
+++ chat_nestjs/src/chat/schemas/chat.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type chatDocument = HydratedDocument<Chat>;
+
+@Schema()
+export class Chat {
+  @Prop({ required: true })
+  roomId: string;
+
+  @Prop({ required: true })
+  userId: number;
+
+  @Prop({ required: true })
+  recipientId: number;
+
+  @Prop({ required: true })
+  message: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+}
